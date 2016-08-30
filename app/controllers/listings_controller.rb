bebbc5e19c37428643c04be05e63c28bf7044689
Nathan Watkins <nathanwatkins@mac.com>
2016-08-30 14:46:25 -0700

    Lastest changes from Nate

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,12 +2,21 @@ class ListingsController < ApplicationController
   def new
     @categories = Category.all
     @subcategories = Subcategory.all
+    @states = State.all
   end
 
   def create
+    @listing = Listing.create(listing_params)
+    if @listing.valid?
+      redirect_to "/listings/#{@listing.id}"
+    else
+      flash[:errors] = @listing.errors.full_messages
+      redirect_to "/listings/new"
+    end
   end
 
   def show
+    @listing = Listing.find(params[:id])
   end
 
   def edit
@@ -20,5 +29,11 @@ class ListingsController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :street, :city, :state_id, :zipcode, :category_id, :subcategory_id)
   end
 end
